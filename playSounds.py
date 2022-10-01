ab3b2c6a1c9a8d92e2de7d3a6e54e13980ef8091
Ben Rees <108217171+BRPortfolio@users.noreply.github.com>
2022-10-01 17:51:07 +0100

    3 new sounds

--- playSounds.py
+++ playSounds.py
@@ -1,27 +1,47 @@
 import pygame
 
+
 class sounds:
-    def __init__(self):
+
+    freeChannel = None
+    
+    def __init__():
         pygame.mixer.init()
     def playBG():
         sound = pygame.mixer.Sound("Sounds\\BGmusic.mp3")
         channel = pygame.mixer.Channel(0)
         channel.play(sound, -1)
         channel.set_volume(0.3)
-    def playFireOut():
-        sound = pygame.mixer.Sound("Sounds\\fireOut.mp3")
+    def playGodlike():
+        sound = pygame.mixer.Sound("Sounds\\godlike.mp3")
         channel = pygame.mixer.Channel(1)
         channel.play(sound)
-        channel.set_volume(1.5)
+        channel.set_volume(1)
+    def playLegendary():
+        sound = pygame.mixer.Sound("Sounds\\legendary.mp3")
+        channel = pygame.mixer.Channel(2)
+        channel.play(sound)
+        channel.set_volume(1)
+    def playUnstoppable():
+        sound = pygame.mixer.Sound("Sounds\\unstoppable.mp3")
+        channel = pygame.mixer.Channel(3)
+        channel.play(sound)
+        channel.set_volume(1)
     def playWomanScream():
         sound = pygame.mixer.Sound("Sounds\\screamWoman.mp3")
-        channel = pygame.mixer.Channel(2)
+        channel = pygame.mixer.Channel(4)
         channel.play(sound)
         channel.set_volume(1)
     def playManScream():
         sound = pygame.mixer.Sound("Sounds\\screamMan.mp3")
-        channel = pygame.mixer.Channel(3)
+        channel = pygame.mixer.Channel(5)
         channel.play(sound)
         channel.set_volume(0.5)
+    def playFireOut():
+        #newChannel = pygame.mixer.find_channel()
+        sound = pygame.mixer.Sound("Sounds\\fireOut.mp3")
+        channel = pygame.mixer.Channel(6)
+        channel.play(sound)
+        channel.set_volume(1.5)
     def stopAllSounds():
         pygame.mixer.quit()
