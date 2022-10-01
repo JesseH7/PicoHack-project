8d2e3c6e273b43ad05919f85d3745bdff5e1d3e8
JesseH7 <114806363+JesseH7@users.noreply.github.com>
2022-10-01 13:16:43 +0100

    Assets

--- test.py
+++ test.py
@@ -3,13 +3,15 @@ import pygame
 
 pygame.init()
 
-pygame.display.set_caption('Quick Start')
+pygame.display.set_caption('Whack a Fire!')
 window_surface = pygame.display.set_mode((1600, 1200))
 
 background = pygame.Surface((1600, 1200))
 background.fill(pygame.Color('#000000'))
 
 is_running = True
+houseT = pygame.image.load("C:\Users\jh17g22\OneDrive - University of Southampton\Documents\GitHub\PicoHack-project\Assets\HouseT.png").convert()
+
 
 while is_running:
 
@@ -19,4 +21,20 @@ while is_running:
 
     window_surface.blit(background, (0, 0))
 
+
     pygame.display.update()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
